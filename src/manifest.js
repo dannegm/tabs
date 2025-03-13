54b98f00cbd827195c3fac72c9068777bc0b52e3
dannegm <im@danielgarcia.me>
2025-03-13 04:16:09 -0600

    tweaking manifest

--- src/manifest.js
+++ src/manifest.js
@@ -1,17 +1,24 @@
 import { defineManifest } from '@crxjs/vite-plugin';
 import packageJson from '../package.json';
 
-const { version } = packageJson;
+const { version, description, author } = packageJson;
 const [major, minor, patch, label = '0'] = version.replace(/[^\d.-]+/g, '').split(/[.-]/);
 
-export default defineManifest(async env => ({
+export default defineManifest({
     manifest_version: 3,
-    name: env.mode === 'staging' ? '[INTERNAL] Tabs.' : 'Tabs.',
+    name: 'Tabs.',
+    description,
+    author,
     version: `${major}.${minor}.${patch}.${label}`,
     version_name: version,
     permissions: ['tabs', 'windows'],
 
+    icons: {
+        16: 'icons/icon-16.png',
+        128: 'icons/icon-128.png',
+    },
+
     chrome_url_overrides: {
         newtab: 'index.html',
     },
-}));
+});
