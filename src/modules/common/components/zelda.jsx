a5549953464f4383710f10e0195201d8d639da14
dannegm <im@danielgarcia.me>
2025-05-25 01:29:51 -0600

    Remove unnecessary React import in zelda component

--- src/modules/common/components/zelda.jsx
+++ src/modules/common/components/zelda.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useCopyToClipboard } from '@uidotdev/usehooks';
 import { toast } from 'sonner';
 
